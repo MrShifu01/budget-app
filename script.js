8bff0349a4d625d92cf4605037dd88c23693e1b4
ChristianStander <stander.christian@gmail.com>
2023-05-01 07:00:58 +0200

    finished the App, working great"

--- script.js
+++ script.js
@@ -54,11 +54,11 @@ sessionStorage.setItem(investment.name + '-income', investmentString)
 sessionStorage.setItem(commission.name + '-income', commissionString)
 
 // Expenses
-sessionStorage.setItem(groceries.name, groceriesString)
-sessionStorage.setItem(rentExpense.name, rentExpenseString)
-sessionStorage.setItem(netflix.name, netflixString)
-sessionStorage.setItem(carPayment.name, carPaymentString)
-sessionStorage.setItem(studies.name, studiesString)
+sessionStorage.setItem(groceries.name + '-expense', groceriesString)
+sessionStorage.setItem(rentExpense.name + '-expense', rentExpenseString)
+sessionStorage.setItem(netflix.name + '-expense', netflixString)
+sessionStorage.setItem(carPayment.name + '-expense', carPaymentString)
+sessionStorage.setItem(studies.name + '-expense', studiesString)
 
 // Getting the Nav Items so that I can Add a prompt to it when clicked
 const incomeNav = document.getElementById("income-nav")
@@ -67,45 +67,188 @@ const disopasableIncomeNav = document.getElementById("disposable-income-nav")
 
 // Adding Event Listeners
 incomeNav.addEventListener('click', showIncomePrompt)
-// expenseNav.addEventListener('click', showExpensePrompt)
-// disopasableIncomeNav.addEventListener('click', showDisposableIncomePrompt)
+expenseNav.addEventListener('click', showExpensePrompt)
+disopasableIncomeNav.addEventListener('click', showDisposableIncomePrompt)
 
 // Income
 function showIncomePrompt () {
 
+    // Looping through the sessionStorage items and converting it back to an object
     const incomeList = []
     for (let i = 0; i < sessionStorage.length; i++) {
         if(sessionStorage.key(i).includes("-income")) {
             const tempObj = JSON.parse(sessionStorage.getItem(sessionStorage.key(i)))
-            for (let key in tempObj) {
-                let value = tempObj[key]
-                incomeList.push(value)
-            }
+            incomeList.push(tempObj)
         }
     }
 
+    // Formatting the IncomeList
+    let formattedIncomeList = 'Name       Amount       Recurring\n'
+    formattedIncomeList += '------------------------------------\n'
+    incomeList.forEach(income => {
+        formattedIncomeList += `${income.name}: R${income.amount}---${income.recurring}\n`
+    })
+
     // Adding the Income Inforamtion to the prompt and asking for more income
-    const moreIncome = prompt(`${incomeList}\nAdd Another Income? y/n`)
+    const moreIncome = prompt(`${formattedIncomeList}\nAdd Another Income? y/n`)
 
     // Logic to ask a user for all the information of the new income
     const incorrectResponse = "You gave an incorrect response, try again!"
+    const emptyResponse = "Value can't be empty, please try again!"
     if (moreIncome === 'n') {
         alert("Click back on Income on the page to add more another income")
     } else if (moreIncome === 'y') {
-        const newIncome = prompt("What is the name of the new Income?")
-        const newAmount = prompt("What is the amount of that income? (R)")
+        const newIncome = prompt("What is the name of the new income?")
+            if (newIncome === null || newIncome === "") {
+                alert(emptyResponse)
+                return
+            }
+        const newAmount = Number(prompt("What is the amount of that income? (R)"))
+            if (newAmount === null || newAmount === "") {
+                alert(emptyResponse)
+                return
+            } else if (isNaN(newAmount)) {
+                alert(incorrectResponse)
+                return
+            }
         let newRecurring = prompt("Is the income recurring? y/n")
+            if (newRecurring === null || newRecurring === "") {
+                alert(emptyResponse)
+                return
+            }
         if (newRecurring === 'n') {
             newRecurring = false
         } else if (newRecurring === 'y') {
             newRecurring = true
-        } else {
+        } else if (newRecurring != 'n' || newRecurring != 'y' || newRecurring != null || newRecurring != '') {
             alert(incorrectResponse)
+            return
         }
+
+        // creating the new object and adding it to sessionStorage via JSON
         const addedIncome = new Income(newIncome, newAmount, newRecurring)
         const addedIncomeString = JSON.stringify(addedIncome)
         sessionStorage.setItem(addedIncome.name + '-income', addedIncomeString )
+    } else if (moreIncome === null || moreIncome === "") {
+        return
     } else {
         alert(incorrectResponse)
+        return
+    }
+}
+
+// Expenses
+function showExpensePrompt () {
+
+    // Looping through the sessionStorage items and converting it back to an object
+    const expenseList = []
+    for (let i = 0; i < sessionStorage.length; i++) {
+        if(sessionStorage.key(i).includes("-expense")) {
+            const tempObj = JSON.parse(sessionStorage.getItem(sessionStorage.key(i)))
+            expenseList.push(tempObj)
+        }
+    }
+
+    // Formatting the expenseList
+    let formattedExpenseList = 'Name       Amount       Recurring\n'
+    formattedExpenseList += '------------------------------------\n'
+    expenseList.forEach(expense => {
+        formattedExpenseList += `${expense.name}: R${expense.amount}---${expense.recurring}\n`
+    })
+
+    // Adding the expense Inforamtion to the prompt and asking for more expense
+    const moreExpense = prompt(`${formattedExpenseList}\nAdd Another expense? y/n`)
+
+    // Logic to ask a user for all the information of the new expense
+    const incorrectResponse = "You gave an incorrect response, try again!"
+    const emptyResponse = "Value can't be empty, please try again!"
+    if (moreExpense === 'n') {
+        alert("Click back on Income on the page to add more another expense")
+    } else if (moreExpense === 'y') {
+        const newExpense = prompt("What is the name of the new expense?")
+            if (newExpense === null || newExpense === "") {
+                alert(emptyResponse)
+                return
+            }
+        const newAmount = Number(prompt("What is the amount of that expense? (R)"))
+            if (newAmount === null || newAmount === "") {
+                alert(emptyResponse)
+                return
+            } else if (isNaN(newAmount)) {
+                alert(incorrectResponse)
+                return
+            }
+        let newRecurring = prompt("Is the expense recurring? y/n")
+            if (newRecurring === null || newRecurring === "") {
+                alert(emptyResponse)
+                return
+            }
+        if (newRecurring === 'n') {
+            newRecurring = false
+        } else if (newRecurring === 'y') {
+            newRecurring = true
+        } else if (newRecurring != 'n' || newRecurring != 'y' || newRecurring != null || newRecurring != '') {
+            alert(incorrectResponse)
+            return
+        }
+
+        // creating the new object and adding it to sessionStorage via JSON
+        const addedExpense = new Expenses(newExpense, newAmount, newRecurring)
+        const addedExpenseString = JSON.stringify(addedExpense)
+        sessionStorage.setItem(addedExpense.name + '-expense', addedExpenseString )
+    } else if (moreExpense === null || moreExpense === "") {
+        return
+    } else {
+        alert(incorrectResponse)
+    }
+}
+
+// Disposable Income
+function showDisposableIncomePrompt () {
+
+    // Looping through the sessionStorage items and converting it back to an object
+    const incomeList = []
+    for (let i = 0; i < sessionStorage.length; i++) {
+        if(sessionStorage.key(i).includes("-income")) {
+            const tempIncObj = JSON.parse(sessionStorage.getItem(sessionStorage.key(i)))
+            incomeList.push(tempIncObj)
+        }
+    }
+
+    // Calculate total income
+    let totalIncome = 0
+    incomeList.forEach(income => {
+        totalIncome += income.amount
+    })
+
+    // Looping through the sessionStorage items and converting it back to an object
+    const expenseList = []
+    for (let i = 0; i < sessionStorage.length; i++) {
+        if(sessionStorage.key(i).includes("-expense")) {
+            const tempExpObj = JSON.parse(sessionStorage.getItem(sessionStorage.key(i)))
+            expenseList.push(tempExpObj)
+        }
+    }
+
+    // Calculate total Expenses
+    let totalExpenses = 0
+    expenseList.forEach(expense => {
+        totalExpenses += expense.amount
+    })
+
+    // Calculate and Display equation for disposable Income
+    const savings = prompt(`Disposable Income = Total Income: R${totalIncome} - Total Expenses: R${totalExpenses} = R${totalIncome - totalExpenses}\nHow much of you disposable income do you want to put into savings?`)
+
+    if (savings === 0 || savings === null || savings === "") {
+        alert(`Disposable Income Left: R${totalIncome - totalExpenses - savings}`)
+        return
+    } else if (isNaN(savings)) {
+        alert("Please try again, but this time enter a number")
+        return
+    } else if (savings > totalIncome - totalExpenses) {
+        alert("You can't save more than you have!")
+    } else {
+        alert(`Disposable Income Left: R${totalIncome - totalExpenses - savings}`)
+        return
     }
 }
